a455df8ad40c60c594fc765137337ceb0617710c
Dario-Pinzauti <pinzautid@gmail.com>
2023-12-26 16:43:49 +0100

    feature: first commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+const emberNumber = ref(0)
+const keyNumber = ref(0)
+const key = reactive(['/keyempty.png','/key1.png','/key2.png','/keyfull.png'])
+
+const increment = () => {
+  emberNumber.value++
+}
+
+const decrement = () => {
+  emberNumber.value--
+}
+
+const keyIncrement = () => {
+  if(keyNumber.value == 3){
+    keyNumber.value = 0
+    return
+  }
+  keyNumber.value++
+}
+const keyDecrement = () => {
+  if(keyNumber.value == 0){
+    keyNumber.value = 3
+    return
+  }
+  keyNumber.value--
+}
+
+const forge= () => {
+  if(emberNumber.value < 6){
+    return
+  }
+  emberNumber.value = emberNumber.value-6 
+  keyNumber.value++
+}
+</script>
+
+<template>
+  <div class="app">
+  <div>
+      <img :src="key[keyNumber]" class="logo" alt="Vite logo" />
+    <div style="text-align: center;">
+      <button @click="keyIncrement">+</button>
+      <button @click="keyDecrement">-</button> 
+    </div>
+
+  </div>
+  <div class="ember">
+      <img src="/ember.png" class="emberImage"  />
+    <div style="text-align: center;">
+      <p>{{ emberNumber }}</p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button> 
+      <button @click="forge">forgia</button> 
+    </div>
+  </div>
+</div>
+</template>
+
+
+<style scoped>
+
+</style>

d92d2974f4623216ec55836b7c525a24856fddad
Gabriel Mihai <mihai95gabrieldev@gmail.com>
2023-12-27 10:11:48 +0100

    Add .gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,61 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive, ref } from 'vue';
 
-const emberNumber = ref(0)
-const keyNumber = ref(0)
-const key = reactive(['/keyempty.png','/key1.png','/key2.png','/keyfull.png'])
+const emberNumber = ref(0);
+const keyNumber = ref(0);
+const key = reactive(['/keyempty.png', '/key1.png', '/key2.png', '/keyfull.png']);
 
 const increment = () => {
-  emberNumber.value++
-}
+  emberNumber.value++;
+};
 
 const decrement = () => {
-  emberNumber.value--
-}
+  emberNumber.value--;
+};
 
 const keyIncrement = () => {
-  if(keyNumber.value == 3){
-    keyNumber.value = 0
-    return
+  if (keyNumber.value == 3) {
+    keyNumber.value = 0;
+    return;
   }
-  keyNumber.value++
-}
+  keyNumber.value++;
+};
 const keyDecrement = () => {
-  if(keyNumber.value == 0){
-    keyNumber.value = 3
-    return
+  if (keyNumber.value == 0) {
+    keyNumber.value = 3;
+    return;
   }
-  keyNumber.value--
-}
+  keyNumber.value--;
+};
 
-const forge= () => {
-  if(emberNumber.value < 6){
-    return
+const forge = () => {
+  if (emberNumber.value < 6) {
+    return;
   }
-  emberNumber.value = emberNumber.value-6 
-  keyNumber.value++
-}
+  emberNumber.value = emberNumber.value - 6;
+  keyNumber.value++;
+};
 </script>
 
 <template>
   <div class="app">
-  <div>
+    <div>
       <img :src="key[keyNumber]" class="logo" alt="Vite logo" />
-    <div style="text-align: center;">
-      <button @click="keyIncrement">+</button>
-      <button @click="keyDecrement">-</button> 
+      <div style="text-align: center">
+        <button @click="keyIncrement">+</button>
+        <button @click="keyDecrement">-</button>
+      </div>
     </div>
-
-  </div>
-  <div class="ember">
-      <img src="/ember.png" class="emberImage"  />
-    <div style="text-align: center;">
-      <p>{{ emberNumber }}</p>
-      <button @click="increment">+</button>
-      <button @click="decrement">-</button> 
-      <button @click="forge">forgia</button> 
+    <div class="ember">
+      <img src="/ember.png" class="emberImage" />
+      <div style="text-align: center">
+        <p>{{ emberNumber }}</p>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+        <button @click="forge">forgia</button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
